23c97cb1394014f01579ec416c9f6658d2aee4d0
Abderrahman <chouia.abderrahman.ad@gmail.com>
2024-09-09 09:55:21 +0100

    Added get and put singular category

--- Django_api/backend/urls.py
+++ Django_api/backend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import create_product, read_product, category_list_create
+from .views import create_product, read_product, category_list_create, edit_fetch_category
 
 urlpatterns = [
     path('products/', create_product, name='create_product'),
+    path('categories/<int:id>', edit_fetch_category, name='edit_fetch_product'),
     path('categories/', category_list_create, name='categories'),
 ]
--- Django_api/backend/views.py
+++ Django_api/backend/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Category
 from .serializers import CategorySerializer
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 @api_view(['GET', 'POST'])
@@ -19,6 +20,24 @@ def category_list_create(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT'])
+def edit_fetch_category(request, id):
+    category = get_object_or_404(Category, id=id)
+
+    if request.method == 'GET':
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = CategorySerializer(category, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def create_product(request):
